c4f6de8f0aa1019101c04540ec6b94d7aa225229
JafarKMarouf <jafar.k.marouf@gmail.com>
2024-09-03 12:12:21 -0400

    add strip package and configure it in android

--- lib/features/checkout/presentation/views/payment_details_view.dart
+++ lib/features/checkout/presentation/views/payment_details_view.dart
@@ -1,16 +1,16 @@
-import 'package:checkout_payment/core/widgets/custome_app_bar.dart';
-import 'package:flutter/material.dart';
+// import 'package:checkout_payment/core/widgets/custome_app_bar.dart';
+// import 'package:flutter/material.dart';
 
-import 'widgets/payment_details_view_body.dart';
+// import 'widgets/payment_details_view_body.dart';
 
-class PaymentDetailsView extends StatelessWidget {
-  const PaymentDetailsView({super.key});
+// class PaymentDetailsView extends StatelessWidget {
+//   const PaymentDetailsView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: customeAppBar(title: 'Payment Details'),
-      body: const PaymentDetailsViewBody(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: customeAppBar(title: 'Payment Details'),
+//       body: const PaymentDetailsViewBody(),
+//     );
+//   }
+// }
--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,58 +1,58 @@
-import 'package:checkout_payment/core/widgets/custome_button.dart';
-import 'package:checkout_payment/features/checkout/presentation/views/custome_credit_card.dart';
-import 'package:checkout_payment/features/checkout/presentation/views/thank_you_view.dart';
-import 'package:flutter/material.dart';
+// import 'package:checkout_payment/core/widgets/custome_button.dart';
+// import 'package:checkout_payment/features/checkout/presentation/views/custome_credit_card.dart';
+// import 'package:checkout_payment/features/checkout/presentation/views/thank_you_view.dart';
+// import 'package:flutter/material.dart';
 
-import 'payment_method_list_view.dart';
+// import 'payment_method_list_view.dart';
 
-class PaymentDetailsViewBody extends StatefulWidget {
-  const PaymentDetailsViewBody({super.key});
+// class PaymentDetailsViewBody extends StatefulWidget {
+//   const PaymentDetailsViewBody({super.key});
 
-  @override
-  State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
-}
+//   @override
+//   State<PaymentDetailsViewBody> createState() => _PaymentDetailsViewBodyState();
+// }
 
-class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const SliverToBoxAdapter(child: PaymentMethodsListView()),
-        SliverToBoxAdapter(
-          child: CustomeCreditCard(
-            formKey: formKey,
-            autovalidateMode: autovalidateMode,
-          ),
-        ),
-        SliverFillRemaining(
-          hasScrollBody: false,
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 12.0, right: 20, left: 20),
-              child: CustomeButton(
-                title: 'Pay',
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    formKey.currentState!.save();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ThankYouView(),
-                      ),
-                    );
-                  } else {
-                    autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
-              ),
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
+// class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
+//   final GlobalKey<FormState> formKey = GlobalKey();
+//   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomScrollView(
+//       slivers: [
+//         const SliverToBoxAdapter(child: PaymentMethodsListView()),
+//         SliverToBoxAdapter(
+//           child: CustomeCreditCard(
+//             formKey: formKey,
+//             autovalidateMode: autovalidateMode,
+//           ),
+//         ),
+//         SliverFillRemaining(
+//           hasScrollBody: false,
+//           child: Align(
+//             alignment: Alignment.bottomCenter,
+//             child: Padding(
+//               padding: const EdgeInsets.only(bottom: 12.0, right: 20, left: 20),
+//               child: CustomeButton(
+//                 title: 'Pay',
+//                 onPressed: () {
+//                   if (formKey.currentState!.validate()) {
+//                     formKey.currentState!.save();
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                         builder: (context) => const ThankYouView(),
+//                       ),
+//                     );
+//                   } else {
+//                     autovalidateMode = AutovalidateMode.always;
+//                     setState(() {});
+//                   }
+//                 },
+//               ),
+//             ),
+//           ),
+//         )
+//       ],
+//     );
+//   }
+// }
